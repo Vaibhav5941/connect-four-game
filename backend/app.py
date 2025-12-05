1fa1eccf54e663ebe59241b5bbcef40b7735bfd3
Vaibhav Gupta <gvaibhav5941@gmail.com>
2025-12-05 17:03:42 +0530

    Fix: downgrade Python version for eventlet

--- backend/app.py
+++ backend/app.py
@@ -153,4 +153,4 @@ def check_winner(board, row, col):
     return None
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5000)
+    socketio.run(app, host='0.0.0.0', port=5000,allow_unsafe_werkzeug=True)
